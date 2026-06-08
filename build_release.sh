cfde929f4c062710f8c2cbb8613d24a591ece2f1
iCrazeiOS <39101269+iCrazeiOS@users.noreply.github.com>
2026-06-08 14:09:48 +0100

    ensure build_release.sh only packages the IPA once

--- build_release.sh
+++ build_release.sh
@@ -6,7 +6,7 @@ make package FINALPACKAGE=1 THEOS_PACKAGE_SCHEME=
 cp -p "`ls -dtr1 packages/* | tail -1`" ./dist/
 
 make clean
-make package FINALPACKAGE=1 THEOS_PACKAGE_SCHEME=rootless
+make package FINALPACKAGE=1 THEOS_PACKAGE_SCHEME=rootless PACKAGE_TIPA=1
 cp -p "`ls -dtr1 packages/* | tail -1`" ./dist/
 
 cp -p TrollDecrypt.tipa ./dist/
